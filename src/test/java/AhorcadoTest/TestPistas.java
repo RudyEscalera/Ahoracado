fa39743713bf05d0c5298e92f71077800a6a6374
RudyEscalera <morbocuby@hotmail.com>
2013-11-14 21:49:42 -0300

    probando otros

--- src/test/java/AhorcadoTest/TestPistas.java
+++ src/test/java/AhorcadoTest/TestPistas.java
@@ -12,5 +12,18 @@ public class TestPistas {
 		int pistasUsadas=6;
 		assertEquals(pistasDisponibles,pistasUsadas);
 	}
+	@Test
+	public void CantidadPistasMedio() {
+		int pistasDisponibles=4;
+		int pistasUsadas=4;
+		assertEquals(pistasDisponibles,pistasUsadas);
+	}
+	@Test
+	public void CantidadPistasAvanzado() {
+		int pistasDisponibles=2;
+		int pistasUsadas=2;
+		assertEquals(pistasDisponibles,pistasUsadas);
+	}
+	
 
 }
